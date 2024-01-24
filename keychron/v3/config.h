31343ef9cacdacc6210cda0624fbe18b1e7441c6
Yann Muller <yann.muller@gmail.com>
2024-01-24 17:52:41 +0000

    cleanup

--- keychron/v3/config.h
+++
@@ -1,8 +0,0 @@
-
-// Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
-
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM 0
-
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
